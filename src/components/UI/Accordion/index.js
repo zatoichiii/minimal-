32ccf8961ea7d40a8c4adc47af84bd875bdf926f
zatoichiii <azovtsevnikita@mail.ru>
2023-12-03 14:29:57 +0300

    final

--- src/components/UI/Accordion/index.js
+++ src/components/UI/Accordion/index.js
@@ -4,9 +4,14 @@ import styles from "./Accordion.module.scss"
 const Accordion = ({ title, description }) => {
     const [open, setOpen] = useState(false)
     return (
-        <div className={styles.accordion} onClick={()=>setOpen(!open)}>
+        <div className={styles.accordion}>
+            <div className={styles.main}>
             <div className={styles.title}>
                 {title}
+            </div>
+            <div className={styles.plus}  onClick={()=>setOpen(!open)} >
+                +
+            </div>
             </div>
             <div className={`${styles.description} ${open ? styles.descriptionOpen : ""}`}>
                 {description}
--- src/components/blocks/Rules/index.js
+++ src/components/blocks/Rules/index.js
@@ -21,11 +21,39 @@ const Rules = () => {
                         Rules
                     </div>
                     <div className={styles.right}>
-                        <Accordion title={"No Genre Restriction"} description={"No Genre RestrictionNo Genre RestrictionNo Genre RestrictionNo Genre Restrictionv"}/>
-                        <Accordion title={"No Genre Restriction"} description={"No Genre RestrictionNo Genre RestrictionNo Genre RestrictionNo Genre Restrictionv"}/>
-                        <Accordion title={"No Genre Restriction"} description={"No Genre RestrictionNo Genre RestrictionNo Genre RestrictionNo Genre Restrictionv"}/>
-                        <Accordion title={"No Genre Restriction"} description={"No Genre RestrictionNo Genre RestrictionNo Genre RestrictionNo Genre Restrictionv"}/>
-
+                        <div className={styles.padd}>
+                        <Accordion title={"Genre Restriction"} description={"All styles and themes are accepted."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Year Limitation"} description={"We accept every year"}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Only Short Film"} description={"Duration Maximum 60 minutes"}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Language Acceptance"} description={"Films and scripts accepted in English, Swedish.Other languages accepted if subtitled in English or Swedish."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Submission Fees"} description={"Non-refundable and non-reimbursable."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Usage Rights"} description={"Participants declare they hold all necessary usage rights for their submitted film. The festival is not responsible for any legal actions arising from rights infringement.Participants agree to receive marketing newsletters and discounts related to film festivals."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Screening Fee"} description={"The festival does not pay a screening fee."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Organizer Rights"} description={"Organizers reserve the right to change, cancel, or reschedule dates, venue, awards, and categories without consent.The festival may include new categories at its discretion."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Promotion of Work"} description={"The goal is to promote participants' work. Participants agree to allow the festival to send press releases about their projects to distributors and festivals for promotional purposes."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"Season Selection"} description={"As a season selection festival, screenings are done through the FilmFreeway project.Participants are encouraged to keep their projects updated.- Winners of main categories are screened at the event."}/>
+                        </div>
+                        <div className={styles.padd}>
+                        <Accordion title={"By submitting "} description={"to Minimal Short Film Festival participants accept all these conditions."}/>
+                        </div>
                     </div>
                 </div>
             </Container>
